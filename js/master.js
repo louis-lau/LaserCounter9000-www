7f03feab982d1cfd025e4ce8bf5496b25aa95c13
Louis Laureys <louis@laureys.me>
2018-10-09 22:02:35 +0200

    Add laser animation on hover

--- js/master.js
+++ js/master.js
@@ -1,20 +1,36 @@
-// smooth scrolling
-$(document).on('click', 'a[href^="#"]', function (event) {
+$(document).ready(function() {
+
+  // smooth scrolling
+  $(document).on('click', 'a[href^="#"]', function(event) {
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+      scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
-});
+  });
 
-var fixedSwitchPosition = $("#landing").height() - 128;
-$( window ).resize(function() {
-  fixedSwitchPosition = $("#landing").height() - 128;
-});
-
-$(window).bind('scroll', function () {
+  var fixedSwitchPosition = $("#landing").height() - 128;
+  $(window).resize(function() {
+    fixedSwitchPosition = $("#landing").height() - 128;
+  });
+  $(window).bind('scroll', function() {
     if ($(window).scrollTop() >= fixedSwitchPosition) {
-        $('nav').addClass('fixed');
+      $('nav').addClass('fixed');
     } else {
-        $('nav').removeClass('fixed');
+      $('nav').removeClass('fixed');
     }
+  });
+
+  setTimeout(function() {
+    $(".logo").addClass('laser-on')
+  }, 500);
+  $("#laser-line").hover(
+    function() {
+      $(".logo").removeClass("laser-on");
+    },
+    function() {
+      setTimeout(function() {
+      $(".logo").addClass('laser-on')
+    }, 1000);
+    }
+  );
 });
