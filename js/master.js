57e6de103a90abcb09937a9ba0fc485a1556fbfa
Louis Laureys <louis@laureys.me>
2018-10-09 21:05:47 +0200

    Fancier nav

--- js/master.js
+++ js/master.js
@@ -1,8 +1,20 @@
 // smooth scrolling
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
-
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
+});
+
+var fixedSwitchPosition = $("#landing").height() - 128;
+$( window ).resize(function() {
+  fixedSwitchPosition = $("#landing").height() - 128;
+});
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() >= fixedSwitchPosition) {
+        $('nav').addClass('fixed');
+    } else {
+        $('nav').removeClass('fixed');
+    }
 });
